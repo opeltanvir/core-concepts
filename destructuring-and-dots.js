3b65877313469f0e8e2b0c9c7ab314ff84edb195
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-30 00:15:50 +0600

    destructuring

--- destructuring-and-dots.js
+++ destructuring-and-dots.js
@@ -1,22 +1,22 @@
 //  array destructuring
 const numbers =[44, 19, 100, 587, 4];
-// const x = numbers[0];
-// const y = numbers [1];
-// const [x,y] = [54,99]
+const x = numbers[0];
+const y = numbers [1];
+const [x,y] = [54,99]
 const [x,y] = numbers;
-// console.log(x,y);
+console.log(x,y);
 
 function boxify(num1){
     return [num1, num1+ 5];
 }
  const [box1, box2] = boxify(8);
-//  console.log(box1,box2);
+ console.log(box1,box2);
 
 //  object destructuring 
 
 const {name, age} = { id: 2, money:34, name: 'apple',age:25}
-// const {money} = person;
-// console.log(money);
+const {money} = person;
+console.log(money);
 
 // create object shortcut
 const one = 25;
