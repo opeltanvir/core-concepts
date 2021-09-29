f0d48c7f6bbfd29ff63c9c3f791ac0ced2b1885c
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-29 21:48:29 +0600

    core concepts

---
+++ core-concepts.js
@@ -0,0 +1,47 @@
+// variable: let, const
+let money = 20;
+money = 12;
+const name = 'kudus'
+
+const person = {
+    name: 'kudus'
+    age: 55,
+    city: 'Dhaka'
+}
+const statement = `this person ${name} with age ${person.age}`;
+
+
+// condition
+if(money > 20 && age >=45){
+
+}
+else{
+
+}
+
+// array
+const numbers = [34, 23, 16];
+const respected =['Mother', 'jahangir'];
+const products = [{name: 'laptop',price:400}, {name:mobile,price:500}]
+
+// loop: for loop
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    
+}
+
+// function:regular arrow
+function add (num1, num2){
+    return num1 + num2;
+}
+
+const result = add(45, 65);
+
+// arrow
+const fiveTimes = num => num* 5;
+const add2 = (num1, num2) => num1 + num2;
+const doMath = (x,y) => {
+    const sum = x+y;
+    return sum * 2;
+}
